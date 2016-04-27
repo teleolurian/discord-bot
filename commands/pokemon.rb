4ac1a99b33819289808d5b4458df3eb7f737d268
Teleolurian Kordyne <teleolurian@propertyline.com>
2016-04-27 11:05:01 -0700

    Command update

--- commands/pokemon.rb
+++ commands/pokemon.rb
@@ -6,7 +6,7 @@ class SisterMercy::Commands::Pokemon < SisterMercy::Command
 
 
   def execute(event, nm)
-    pokemon = get_pokemon(nm)
+    pokemon = get_pokemon(nm.downcase)
     types = pokemon.types.map {|x| x.type.name}.join ?/
     abilities = pokemon.abilities.map {|x| x.ability.name}.join ', '
     "#{pokemon.name} (#{types}) - #{abilities}"
