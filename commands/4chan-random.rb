cb24ee2a72feaa82afb28a5e0c050d9d6f03a1d1
Teleolurian Kordyne <teleolurian@gmail.com>
2016-05-19 18:19:11 -0700

    4chan random post

--- commands/4chan-random.rb
+++ commands/4chan-random.rb
@@ -1,9 +1,9 @@
 require './commands/4chan'
 class SisterMercy::Commands::ChanAn < ChanCommand
-  def self.name; :an; end
+  def self.name; :4chan; end
 
-  def random_an_post
-    board       = get_board('an')
+  def random_post(b = 'an')
+    board       = get_board(b)
     threadlist  = board.threads
     threadlist.shift
 
@@ -11,17 +11,17 @@ class SisterMercy::Commands::ChanAn < ChanCommand
     post        = thread.posts.random
     result = +post.com.gsub(/<br[^>]*>/, "\n").gsub(/<a.*?href=(\S+)[^>]+>(.*?)<\/a>/, "\\2 (\\1)").gsub(/<[^>]+>/, '')
     if post.tim
-      result += "  http://i.4cdn.org/an/#{post.tim}#{post.ext}"
+      result += "  http://i.4cdn.org/#{b}/#{post.tim}#{post.ext}"
     end
     result
   end
 
   def description
-    '/an/ posts'
+    'random 4chan posts with picture'
   end
 
-  def execute(event)
-    random_an_post
+  def execute(event, board = 'an')
+    random_post(board)
   end
 end
 
