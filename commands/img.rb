338296d46c0ed5ae3a9e008a96ae0b157afe44d0
Sekeol Kim <teleolurian@gmail.com>
2017-04-13 18:40:38 -0700

    Update img.rb

--- commands/img.rb
+++ commands/img.rb
@@ -5,10 +5,10 @@ class SisterMercy::Commands::Img < SisterMercy::Command
 
 
   def execute(event, *token)
-    url = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag={CGI.escape token*' '}"
+    url = "http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=#{token * ?+}"
     begin
-      data = get_json_from url
-      image = data.image_url
+      data = get_json_from(url).data
+      image = data[(rand * data.length).to_i].images.fixed_width.url
       return "I couldn't find anything..." unless image && image.length > 0
       image
     rescue
