245fb3574e2657c92e12b7e2cfb28308cfc70fae
Sekeol Kim <sekkim@groupon.com>
2018-05-23 11:18:38 -0700

    Add RXNav basic interactions

--- commands/pharms.rb
+++ commands/pharms.rb
@@ -22,8 +22,8 @@ class SisterMercy::Commands::Rx < SisterMercy::Commands
   def interactions(*args)
     drug1, drug2 = args.join(' ').split('+')
     return "Syntax: interactions :drug1 + :drug2" unless drug1 && drug2
-    return "Unknown drug #{drug1}" unless rc1 = rxcui_for drug1
-    return "Unknown drug #{drug2}" unless rc2 = rxcui_for drug2
+    return "Unknown drug #{drug1}" unless rc1 = rxcui_for(drug1)
+    return "Unknown drug #{drug2}" unless rc2 = rxcui_for(drug2)
     res = get_rxnav '/interaction/interaction.json?sources=drugbank&rxcui=' + rc1
     ssp = res.interactionTypeGroup.first.interactionType.first.interactionPair rescue []
     result = ssp.find {|ip| (ip.interactionConcept.last.minConceptItem.rxcui rescue FalseClass) == rc2 }
@@ -40,6 +40,4 @@ class SisterMercy::Commands::Rx < SisterMercy::Commands
       "I don't have that power yet... but I'm studying!"
     end
   end
-
-
 end
