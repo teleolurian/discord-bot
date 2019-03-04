881c6cc3ed553d6e22872eed1defc314fd5b2bdc
Sekeol Kim <sekkim@groupon.com>
2019-03-04 10:27:50 -0800

    Add tenor gifs

---
+++ commands/gif.rb
@@ -0,0 +1,18 @@
+require 'uri'
+
+class SisterMercy::Commands::Gif < SisterMercy::Command
+  def self.name; :gif ; end
+
+
+  def execute(event, *imagesearch)
+    imagesearch ?= %w{ wtf }
+    imagesearch.join!('-')
+    url = "http://www.tenor.com/search/" + URI.encode(book)
+    begin
+      h = hpricot(url)
+      (h / '#view .gallery-container .GifList .GifListItem a').random['href']
+    rescue
+      "I... I'll try harder next time..."
+    end
+  end
+end
